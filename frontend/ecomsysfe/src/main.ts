d8ba4a8ef8410abde9add787211d680ad85e5215
DavinHHiu <honghieu2208@gmail.com>
2024-07-28 09:01:20 +0700

    A: config i18n plugin

--- frontend/ecomsysfe/src/main.ts
+++ frontend/ecomsysfe/src/main.ts
@@ -3,6 +3,7 @@ import './assets/css/tailwind.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import i18n from '@/utils/i18n';
 
 import App from './App.vue';
 import router from './router';
@@ -10,6 +11,7 @@ import router from './router';
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(i18n);
 app.use(router);
 
 app.mount('#app');
